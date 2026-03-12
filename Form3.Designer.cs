7264dee58da9a3d159f187b204b6bb91e8ba7c4e
aeronlargo69-debug <aeronlargo69@gmail.com>
2026-03-12 20:47:15 +0800

    Add files via upload

---
+++ Form3.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace calcu
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            RESULTBOX = new TextBox();
+            CLEARENTRY = new Button();
+            button3 = new Button();
+            EVALUATION = new Button();
+            numbtn3 = new Button();
+            numbtn2 = new Button();
+            numbtn1 = new Button();
+            button8 = new Button();
+            numbtn6 = new Button();
+            numbtn5 = new Button();
+            numbtn4 = new Button();
+            button12 = new Button();
+            numbtn9 = new Button();
+            numbtn8 = new Button();
+            numbtn7 = new Button();
+            button16 = new Button();
+            button20 = new Button();
+            button2 = new Button();
+            button17 = new Button();
+            button18 = new Button();
+            numbtn0 = new Button();
+            SuspendLayout();
+            // 
+            // RESULTBOX
+            // 
+            RESULTBOX.Cursor = Cursors.Hand;
+            RESULTBOX.Location = new Point(70, 123);
+            RESULTBOX.MaxLength = 40000;
+            RESULTBOX.Name = "RESULTBOX";
+            RESULTBOX.Size = new Size(247, 23);
+            RESULTBOX.TabIndex = 0;
+            // 
+            // CLEARENTRY
+            // 
+            CLEARENTRY.Cursor = Cursors.Hand;
+            CLEARENTRY.Location = new Point(119, 461);
+            CLEARENTRY.Name = "CLEARENTRY";
+            CLEARENTRY.Size = new Size(74, 52);
+            CLEARENTRY.TabIndex = 2;
+            CLEARENTRY.Text = "CE";
+            CLEARENTRY.UseVisualStyleBackColor = true;
+            CLEARENTRY.Click += CLEARENTRY_Click;
+            // 
+            // button3
+            // 
+            button3.Cursor = Cursors.Hand;
+            button3.Location = new Point(199, 461);
+            button3.Name = "button3";
+            button3.Size = new Size(74, 52);
+            button3.TabIndex = 4;
+            button3.Text = ".";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // EVALUATION
+            // 
+            EVALUATION.Cursor = Cursors.Hand;
+            EVALUATION.Location = new Point(278, 461);
+            EVALUATION.Name = "EVALUATION";
+            EVALUATION.Size = new Size(74, 52);
+            EVALUATION.TabIndex = 5;
+            EVALUATION.Text = "=";
+            EVALUATION.UseVisualStyleBackColor = true;
+            EVALUATION.Click += button4_Click;
+            // 
+            // numbtn3
+            // 
+            numbtn3.Cursor = Cursors.Hand;
+            numbtn3.Location = new Point(39, 405);
+            numbtn3.Name = "numbtn3";
+            numbtn3.Size = new Size(74, 52);
+            numbtn3.TabIndex = 6;
+            numbtn3.Text = "3";
+            numbtn3.UseVisualStyleBackColor = true;
+            numbtn3.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn2
+            // 
+            numbtn2.Cursor = Cursors.Hand;
+            numbtn2.Location = new Point(119, 405);
+            numbtn2.Name = "numbtn2";
+            numbtn2.Size = new Size(74, 52);
+            numbtn2.TabIndex = 7;
+            numbtn2.Text = "2";
+            numbtn2.UseVisualStyleBackColor = true;
+            numbtn2.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn1
+            // 
+            numbtn1.Cursor = Cursors.Hand;
+            numbtn1.Location = new Point(199, 405);
+            numbtn1.Name = "numbtn1";
+            numbtn1.Size = new Size(74, 52);
+            numbtn1.TabIndex = 8;
+            numbtn1.Text = "1";
+            numbtn1.UseVisualStyleBackColor = true;
+            numbtn1.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button8
+            // 
+            button8.Cursor = Cursors.Hand;
+            button8.Location = new Point(279, 384);
+            button8.Name = "button8";
+            button8.Size = new Size(74, 73);
+            button8.TabIndex = 9;
+            button8.Text = "-";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn6
+            // 
+            numbtn6.Cursor = Cursors.Hand;
+            numbtn6.Location = new Point(39, 347);
+            numbtn6.Name = "numbtn6";
+            numbtn6.Size = new Size(74, 52);
+            numbtn6.TabIndex = 10;
+            numbtn6.Text = "6";
+            numbtn6.UseVisualStyleBackColor = true;
+            numbtn6.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn5
+            // 
+            numbtn5.Cursor = Cursors.Hand;
+            numbtn5.Location = new Point(119, 347);
+            numbtn5.Name = "numbtn5";
+            numbtn5.Size = new Size(74, 52);
+            numbtn5.TabIndex = 11;
+            numbtn5.Text = "5";
+            numbtn5.UseVisualStyleBackColor = true;
+            numbtn5.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn4
+            // 
+            numbtn4.Cursor = Cursors.Hand;
+            numbtn4.Location = new Point(199, 347);
+            numbtn4.Name = "numbtn4";
+            numbtn4.Size = new Size(74, 52);
+            numbtn4.TabIndex = 12;
+            numbtn4.Text = "4";
+            numbtn4.UseVisualStyleBackColor = true;
+            numbtn4.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button12
+            // 
+            button12.Cursor = Cursors.Hand;
+            button12.Location = new Point(279, 308);
+            button12.Name = "button12";
+            button12.Size = new Size(74, 70);
+            button12.TabIndex = 13;
+            button12.Text = "+";
+            button12.UseVisualStyleBackColor = true;
+            button12.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn9
+            // 
+            numbtn9.Cursor = Cursors.Hand;
+            numbtn9.Location = new Point(39, 289);
+            numbtn9.Name = "numbtn9";
+            numbtn9.Size = new Size(74, 52);
+            numbtn9.TabIndex = 14;
+            numbtn9.Text = "9";
+            numbtn9.UseVisualStyleBackColor = true;
+            numbtn9.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn8
+            // 
+            numbtn8.Cursor = Cursors.Hand;
+            numbtn8.Location = new Point(119, 289);
+            numbtn8.Name = "numbtn8";
+            numbtn8.Size = new Size(74, 52);
+            numbtn8.TabIndex = 15;
+            numbtn8.Text = "8";
+            numbtn8.UseVisualStyleBackColor = true;
+            numbtn8.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn7
+            // 
+            numbtn7.Cursor = Cursors.Hand;
+            numbtn7.Location = new Point(199, 289);
+            numbtn7.Name = "numbtn7";
+            numbtn7.Size = new Size(74, 52);
+            numbtn7.TabIndex = 16;
+            numbtn7.Text = "7";
+            numbtn7.UseVisualStyleBackColor = true;
+            numbtn7.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button16
+            // 
+            button16.Cursor = Cursors.Hand;
+            button16.Location = new Point(279, 231);
+            button16.Name = "button16";
+            button16.Size = new Size(74, 71);
+            button16.TabIndex = 17;
+            button16.Text = "/";
+            button16.UseVisualStyleBackColor = true;
+            button16.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button20
+            // 
+            button20.Cursor = Cursors.Hand;
+            button20.Location = new Point(199, 231);
+            button20.Name = "button20";
+            button20.Size = new Size(74, 52);
+            button20.TabIndex = 21;
+            button20.Text = "*";
+            button20.UseVisualStyleBackColor = true;
+            button20.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button2
+            // 
+            button2.Cursor = Cursors.Hand;
+            button2.Location = new Point(19, 13);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 22;
+            button2.Text = "back";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button17
+            // 
+            button17.Cursor = Cursors.Hand;
+            button17.Location = new Point(119, 231);
+            button17.Name = "button17";
+            button17.Size = new Size(74, 52);
+            button17.TabIndex = 23;
+            button17.Text = "√ ";
+            button17.UseVisualStyleBackColor = true;
+            button17.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // button18
+            // 
+            button18.Cursor = Cursors.Hand;
+            button18.Location = new Point(39, 231);
+            button18.Name = "button18";
+            button18.Size = new Size(74, 52);
+            button18.TabIndex = 24;
+            button18.Text = "% ";
+            button18.UseVisualStyleBackColor = true;
+            button18.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // numbtn0
+            // 
+            numbtn0.Cursor = Cursors.Hand;
+            numbtn0.Location = new Point(39, 461);
+            numbtn0.Name = "numbtn0";
+            numbtn0.Size = new Size(74, 52);
+            numbtn0.TabIndex = 25;
+            numbtn0.Text = "0";
+            numbtn0.UseVisualStyleBackColor = true;
+            numbtn0.Click += APPENDTOCALCULATIONSTRING;
+            // 
+            // Form3
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.HotTrack;
+            ClientSize = new Size(364, 525);
+            Controls.Add(numbtn0);
+            Controls.Add(button18);
+            Controls.Add(button17);
+            Controls.Add(button2);
+            Controls.Add(button20);
+            Controls.Add(button16);
+            Controls.Add(numbtn7);
+            Controls.Add(numbtn8);
+            Controls.Add(numbtn9);
+            Controls.Add(button12);
+            Controls.Add(numbtn4);
+            Controls.Add(numbtn5);
+            Controls.Add(numbtn6);
+            Controls.Add(button8);
+            Controls.Add(numbtn1);
+            Controls.Add(numbtn2);
+            Controls.Add(numbtn3);
+            Controls.Add(EVALUATION);
+            Controls.Add(button3);
+            Controls.Add(CLEARENTRY);
+            Controls.Add(RESULTBOX);
+            Name = "Form3";
+            Text = "S";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox RESULTBOX;
+        private Button CLEARENTRY;
+        private Button button3;
+        private Button EVALUATION;
+        private Button numbtn3;
+        private Button numbtn2;
+        private Button numbtn1;
+        private Button button8;
+        private Button numbtn6;
+        private Button numbtn5;
+        private Button numbtn4;
+        private Button button12;
+        private Button numbtn9;
+        private Button numbtn8;
+        private Button numbtn7;
+        private Button button16;
+        private Button button20;
+        private Button button2;
+        private Button button17;
+        private Button button18;
+        private Button numbtn0;
+    }
+}
---
+++ Form3.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace calcu
+{
+    public partial class Form3 : Form
+    {
+        string num = "";
+        int num1 = 0;
+        int num2 = 0;
+        string op = "";
+
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Form2 form2 = new Form2();
+            form2.Show();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void APPENDTOCALCULATIONSTRING(object sender, EventArgs e)
+        {
+            Button invokedbtn = sender as Button;
+            if (invokedbtn != null)
+            {
+                RESULTBOX.Text += invokedbtn.Text;
+            }
+        }
+
+        private void CLEARENTRY_Click(object sender, EventArgs e)
+        {
+            RESULTBOX.Text = string.Empty;
+        }
+
+        private void button4_Click(object sender, EventArgs e) //EVALUATION
+        {
+            string expression = RESULTBOX.Text;
+            var result = new DataTable();
+            try
+            {
+                double evalresult1 = Convert.ToDouble(result.Compute(expression, null));
+                if (double.IsInfinity(evalresult1) || double.IsNaN(evalresult1))
+                {
+                    MessageBox.Show("Expression was evaluated to be undefined.", "Evaluation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    RESULTBOX.Text = string.Empty;
+                    return;
+                }
+                RESULTBOX.Text = evalresult1.ToString();
+            }
+            catch (System.Data.SyntaxErrorException)
+            {
+                MessageBox.Show("Expression was not valid.", "Syntax Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                RESULTBOX.Text = string.Empty;
+            }
+        }
+
+    }
+
+}
+
